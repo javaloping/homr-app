cd212ccc514b77177b33b235854bc952db9a311f
Victor Miranda <victomiranda@gmail.com>
2015-07-21 17:57:02 +0100

    #3 Example of repository and model classes

---
+++ src/main/java/com/javaloping/homr/app/model/Property.java
@@ -0,0 +1,34 @@
+package com.javaloping.homr.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author victormiranda@gmail.com
+ */
+@Entity
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/javaloping/homr/app/repository/PropertyRepository.java
@@ -0,0 +1,10 @@
+package com.javaloping.homr.app.repository;
+
+import com.javaloping.homr.app.model.Property;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author victormiranda@gmail.com
+ */
+public interface PropertyRepository extends CrudRepository<Property, Integer> {
+}
