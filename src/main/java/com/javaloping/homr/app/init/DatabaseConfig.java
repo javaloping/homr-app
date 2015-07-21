9244f153f761bd36ceb65c84a322da26fecdb282
Victor Miranda <victomiranda@gmail.com>
2015-07-21 17:58:47 +0100

    #3 Environment is not needed

--- src/main/java/com/javaloping/homr/app/init/DatabaseConfig.java
+++ src/main/java/com/javaloping/homr/app/init/DatabaseConfig.java
@@ -4,13 +4,11 @@ import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -21,9 +19,6 @@ import java.util.Properties;
 @Configuration
 @EnableJpaRepositories("com.javaloping.homr.app.repository")
 public class DatabaseConfig {
-
-    @Resource
-    private Environment env;
 
     @Value("${db.url}")
     private String jdbcUrl;
