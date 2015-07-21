65ac14a6bb0985c216706b80845b65a17f0e4133
Victor Miranda <victomiranda@gmail.com>
2015-07-21 17:22:33 +0100

    #1 Initial integration test for spring mvc

---
+++ src/integration/java/PlaceholderStartTest.java
@@ -0,0 +1,47 @@
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author victormiranda@gmail.com
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = { TestApp.class})
+public class PlaceholderStartTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+    protected MockMvc mock;
+
+    @Before
+    public void setUp() {
+        mock = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void testPlaceholder() throws Exception {
+        final MvcResult resultPublish = mock.perform(
+                post("/user/placeholder")
+                        .accept(MediaType.TEXT_PLAIN)
+        ).andDo(print()).andExpect(status().isOk()).andReturn();
+
+        Assert.assertEquals("This is a placeholder", resultPublish.getResponse().getContentAsString());
+    }
+
+}
---
+++ src/integration/java/TestApp.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author victormiranda@gmail.com
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.javaloping.homr.app")
+@PropertySource("classpath:application-test.properties")
+public class TestApp extends WebMvcConfigurerAdapter {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
