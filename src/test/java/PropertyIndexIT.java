a583b63e6b421aa1596cd153b43fec2ac8386c78
Victor Miranda <victor.mirandabeltran@sledger.net>
2015-07-22 11:15:42 +0100

    #8 test elastic search infraestructure

---
+++ src/test/java/PropertyIndexIT.java
@@ -0,0 +1,41 @@
+import com.javaloping.homr.app.model.Property;
+import com.javaloping.homr.app.model.RentProperty;
+import com.javaloping.homr.app.search.PropertySearcher;
+import com.javaloping.homr.app.type.PropertyType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+/**
+ * @author victormiranda@gmail.com
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = { TestApp.class})
+public class PropertyIndexIT {
+
+    @Autowired
+    private PropertySearcher propertySearcher;
+
+    @Test
+    public void testSimpleIndex() throws IOException {
+        Property property = new RentProperty();
+
+        property.setId(1);
+        property.setPrice(BigDecimal.ONE);
+        property.setName("Fantastic rent");
+        property.setType(PropertyType.FLAT);
+
+        propertySearcher.index(property);
+
+        Object res = propertySearcher.fetch(1);
+
+    }
+}
