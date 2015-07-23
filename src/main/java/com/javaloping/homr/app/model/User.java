bac7d4514e07c642d2a681db70438684b15b972b
Victor Miranda <victomiranda@gmail.com>
2015-07-23 10:41:36 +0100

    #9 User fields for auth

--- src/main/java/com/javaloping/homr/app/model/User.java
+++ src/main/java/com/javaloping/homr/app/model/User.java
@@ -11,6 +11,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    private String username;
+
+    private String password;
+
+    private String email;
+
     @Embedded
     private EntityModification entityModification;
 
@@ -20,6 +26,30 @@ public class User {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public EntityModification getEntityModification() {
