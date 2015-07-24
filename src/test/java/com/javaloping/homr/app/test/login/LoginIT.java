f4df6b625a7def8749d0062d65ade19d5f5985ce
Victor Miranda <victomiranda@gmail.com>
2015-07-24 11:31:02 +0100

    #9 Test invalid login

--- src/test/java/com/javaloping/homr/app/test/login/LoginIT.java
+++ src/test/java/com/javaloping/homr/app/test/login/LoginIT.java
@@ -6,12 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -44,5 +46,13 @@ public class LoginIT {
         ).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"));
+    }
+
+    @Test(expected = NestedServletException.class)
+    public void testLoginFailure() throws Exception {
+        mock.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"username\":\"victor\",\"password\":\"wrong\"}")
+        ).andDo(print());
     }
 }
